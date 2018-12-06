4946b21952326b09c15fd24dab52cb68367638f2
vicsergev <v.s.sergevnin@gmail.com>
2018-12-06 17:46:25 +0300

    Now application solves any system but those which have infinite solutions

--- gauss1.py
+++ gauss1.py
@@ -23,7 +23,6 @@ def gauss_solve(n, m):
 
         # проверка на нулевой элемент: меняем местами с последующими уравнениями
         p = i + 1
-
         try:
             while abs(system[i][i]) < 0.0000001:
                 if p < n:
@@ -41,7 +40,7 @@ def gauss_solve(n, m):
             continue
 
         for j in range(i, n):
-            print(i)
+            # print(i)
 
             if abs(system[i - 1][i - 1]) < 0.0000001:
                 p = i
@@ -52,7 +51,7 @@ def gauss_solve(n, m):
                     else:
                         break
 
-            print(system)
+            # print(system)
             coef = system[j][i - 1] / system[i - 1][i - 1]
 
             if abs(system[j][i - 1]) > 0.0000001:
@@ -61,24 +60,47 @@ def gauss_solve(n, m):
 
     print(system)
 
-    for i in range(n - 1, -1, -1):
-        # проверка на обнулившиеся уравнения
-        if abs(system[i][m - 1]) < 0.0000001 and abs(system[i][m]) > 0.0000001:
-            # если получили уравнение типа 0 = а
-            return 'NO'
-            break
+    # for i in range(n - 1, -1, -1):
+    #     # проверка на обнулившиеся уравнения
+    #     if abs(system[i][m - 1]) < 0.0000001 and abs(system[i][m]) > 0.0000001:
+    #         # если получили уравнение типа 0 = а
+    #         return 'NO'
+    #         break
 
-        elif abs(system[i][m - 1]) < 0.0000001 and abs(system[i - 1][m]) < 0.0000001:
+    #     elif abs(system[i][m - 1]) < 0.0000001 and abs(system[i - 1][m]) < 0.0000001:
+    #         # если получили уравнение типа 0 = 0
+    #         system.pop(i)
+    #         n -= 1
+
+    # проверка на обнулившиеся уравнения
+    for eq in range(n - 1, -1, -1):
+        # если в предыдущем уравнении обнулились сразу несколько элементов
+        nonzerocount = m
+        for elem in range(m):
+            if abs(system[eq][elem]) < 0.0000001:
+                nonzerocount -= 1
+                if nonzerocount == 0:
+                    system.pop(eq)
+                    n -= 1
+            else:
+                nonzero_index = elem
             # если получили уравнение типа 0 = 0
-            system.pop(i)
+        if nonzerocount == 1 and nonzero_index == m - 1:
+            return 'NO'
 
-# обратный ход метода Гаусса
+    # todo: сделать проверку на бесконечное количество решений
 
-# answer = []
+    # обратный ход метода Гаусса
 
-# for i in range(n-1, -1, -1):
+    answer = []
 
-#     x = system[i][]
+    for i in range(n):
+
+        x = (system[n - i - 1][m] - sum(list(map(lambda x, y: x * y, system[n - i - 1][m - 1: m - i - 1 : -1], answer)))) / system[n - i - 1][m - i - 1] # домножать на х
+
+        answer.append(x)
+
+    return answer
 
 
 
